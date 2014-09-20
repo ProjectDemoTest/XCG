3a2dbbf5f83acf1999fde992019143cd0dff6e87
appale <appale@appaledeMac-mini.local>
2014-09-20 16:46:52 +0800

    *first commit two class className XIacg

---
+++ ONEGame/ONEGame/XIacg.h
@@ -0,0 +1,13 @@
+//
+//  XIacg.h
+//  ONEGame
+//
+//  Created by appale on 14-9-20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XIacg : NSObject
+
+@end
---
+++ ONEGame/ONEGame/XIacg.m
@@ -0,0 +1,13 @@
+//
+//  XIacg.m
+//  ONEGame
+//
+//  Created by appale on 14-9-20.
+//
+//
+
+#import "XIacg.h"
+
+@implementation XIacg
+
+@end
